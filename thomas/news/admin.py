95366254ba5fadc77498630098cd23c1b6173f1a
Jensen Hussey <jensechu@gmail.com>
2011-10-18 17:23:43 -0400

    Created the thomas django project. Started the news app

---
+++ thomas/news/admin.py
@@ -0,0 +1,7 @@
+from hoopoid.news.models import News
+from django.contrib import admin
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'article', 'pub_date')
+
+admin.site.register(News, NewsAdmin)
