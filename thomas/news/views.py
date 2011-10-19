21272069d653d5594a6fffd92cfb04d5306f523f
Jensen Hussey <jensechu@gmail.com>
2011-10-19 11:57:34 -0400

    Created base.html

--- thomas/news/views.py
+++ thomas/news/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.template import Context, loader
+from thomas.news.models import News
+from django.http import HttpResponse
+
+def home(request):
+    latest_news_list = News.objects.all().order_by('pub_date')[:5]
+    t = loader.get_template('base.html')
+    c = Context({
+            'latest_news_list': latest_news_list,
+            })
+    return HttpResponse(t.render(c))
--- thomas/urls.py
+++ thomas/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,7 +7,19 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'thomas.views.home', name='home'),
     # url(r'^thomas/', include('thomas.foo.urls')),
-
+                       
+    url(r'^$', 'news.views.home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+
 )
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                    'document_root': settings.MEDIA_ROOT,
+        }),
+    )
